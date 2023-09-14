47685880a26989e914b300ba34e355f1134bd083
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-09-14 18:02:25 +0500

    Task7

---
+++ Tyuiu.AhirbakievAR.Sprint0.Task7.V1.Lib/Tyuiu.AhirbakievAR.Sprint0.Task7.V1.Lib.cpp
@@ -0,0 +1,10 @@
+// Tyuiu.AhirbakievAR.Sprint0.Task7.V1.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+// TODO: This is an example of a library function
+void fnTyuiuAhirbakievARSprint0Task7V1Lib()
+{
+}
---
+++ Tyuiu.AhirbakievAR.Sprint0.Task7.V1.Test/Tyuiu.AhirbakievAR.Sprint0.Task7.V1.Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuAhirbakievARSprint0Task7V1Test
+{
+	TEST_CLASS(TyuiuAhirbakievARSprint0Task7V1Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
